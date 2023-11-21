89de36eefc9cda6792775468ec1fdca252d09bb8
Ahmed Essam <133671053+Glitchv100@users.noreply.github.com>
2023-11-21 17:48:17 +0200

    Add files via upload

--- sweet.js
+++ sweet.js
@@ -2,20 +2,19 @@ const parentBar = document.querySelector(".bar-parent");
 const childCategory = document.getElementById("child-categ");
 const subContainer = document.querySelectorAll(".sub-container");
 const childBar = document.querySelectorAll(".child-bar");
-console.log(childBar , subContainer);
-childBar.forEach((bar ,index)=>{
-    bar.addEventListener("click",(e)=>{
-    console.log("sdjbl");    
-subContainer[index].classList.toggle("deactive")
-    })
-})
+const barIcon = document.querySelectorAll("i");
+console.log(childBar, subContainer);
+childBar.forEach((bar, index) => {
+  bar.addEventListener("click", (e) => {
+    console.log("sdjbl");
+    subContainer[index].classList.toggle("deactive");
+    barIcon[index + 1].classList.toggle("fa-xmark");
+  });
+});
 
-
-
-parentBar.addEventListener("click" , ()=>{
-    childCategory.classList.toggle("deactive")
-} )
-
-
-
-
+parentBar.addEventListener("click", () => {
+  childCategory.classList.toggle("deactive");
+  if (barIcon[0].classList.contains("fa-xmark")) {
+    barIcon[0].classList.replace("fa-xmark", "fa-bars");
+  }
+});
